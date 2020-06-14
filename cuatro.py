a9e0a62dd7ebc71c56087a2ad52984841540db0e
WichoMC <54116406+WichoMC@users.noreply.github.com>
2020-06-14 16:20:45 -0500

    Create cuatro.py

---
+++ cuatro.py
@@ -0,0 +1,23 @@
+
+
+memoria ={1:1,2:1,3:2}
+
+def  fibonacci(numero):
+    a = 1
+    b = 1
+    for i in range (1, numero-1):
+        a,b = b, a+b
+    return b
+
+def fibonacci_top_dowm(numero):
+    if numero in memoria:
+        return memoria[numero]
+    f = fibonacci(numero-1) + fibonacci(numero-2)
+    memoria[numero] = f
+    return memoria[numero]
+
+print(fibonacci_top_dowm(5))
+print(memoria)
+
+print(fibonacci_top_dowm(4))
+print(memoria)
